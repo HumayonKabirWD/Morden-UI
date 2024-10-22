1ea6e831806b79adf6ca7117551731f69d55bcba
kabir <kaabir24124@gmail.com>
2024-10-22 07:04:15 +0600

    this is a masege

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className="">
+            this is a main components
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
